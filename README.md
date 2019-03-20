f6e978901ee5c40474f4440e5ac405bba8fd876e
justcallmegoyo <justcallmegoyo@gmail.com>
2019-03-20 16:05:50 +0000

    set up of rspec and capybara and first test passes

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a project to build the Battle app as part of Week 3 of the Makers Course.
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,20 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+# add this to spec/spec_helper.rb
+
+ENV['RACK_ENV'] = 'test'
+
+# require our Sinatra app file
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+# tell Capybara about our app class
+Capybara.app = Battle
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
