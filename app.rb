65a897fc90eb2810ac17f3528265b6e4a2cd3540
justcallmegoyo <justcallmegoyo@gmail.com>
2019-03-20 16:30:12 +0000

    Added fields for names and posted to new page /names

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-  get '/' do
-    'Testing infrastructure working!'
-  end
+
+get '/' do
+  erb :index
+end
+
+post '/names' do
+  @player_1_name = params[:player_1_name]
+  @player_2_name = params[:player_2_name]
+  erb :play
+end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,9 @@
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit('/')
+    fill_in :player_1_name, with: 'Dave'
+    fill_in :player_2_name, with: 'Mittens'
+    click_button 'Submit'
+    expect(page).to have_content 'Dave vs. Mittens'
+  end
+end
