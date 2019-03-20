74b67a02bcb72131955c5bc7c0d41cfdc16967dd
justcallmegoyo <justcallmegoyo@gmail.com>
2019-03-20 16:46:40 +0000

    post rediirect of player names

--- app.rb
+++ app.rb
@@ -1,16 +1,21 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
 
-get '/' do
-  erb :index
-end
+  get '/' do
+    erb :index
+  end
 
-post '/names' do
-  @player_1_name = params[:player_1_name]
-  @player_2_name = params[:player_2_name]
-  erb :play
-end
+  get '/play' do
+    erb :play
+  end
+
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
