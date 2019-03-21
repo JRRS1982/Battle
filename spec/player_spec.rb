9bdc6b8c4fdc89ccb04d9c4b37bfabb1cc4ca641
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2019-03-21 15:24:59 +0000

    Added player class test passing

---
+++ spec/player_spec.rb
@@ -0,0 +1,13 @@
+require 'player'
+
+describe Player do
+  subject(:dave) { Player.new('Dave') }
+
+  describe '#name' do
+    it 'returns the name' do
+      p dave.name
+      expect(dave.name).to eq 'Dave'
+    end
+  end
+end
+  
